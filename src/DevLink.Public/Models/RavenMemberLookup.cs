d05c8cf920bfb51ea42fc1ae6b0da0eee37ffe52
Kijana Woodard <github@wyldeye.com>
2013-09-09 12:23:29 -0500

    greeting

--- src/DevLink.Public/Models/RavenMemberLookup.cs
+++ src/DevLink.Public/Models/RavenMemberLookup.cs
@@ -6,6 +6,7 @@ namespace DevLink.Public.Models
 	public interface IFindMembers
 	{
 		Member FindMemberByUserName(string username);
+		Member FindMemberById(string id);
 	}
 
 	public class RavenMemberLookup : IFindMembers
@@ -29,6 +30,16 @@ namespace DevLink.Public.Models
 				throw new ApplicationException("Expected valid identifier");
 
 			var member = _session.Load<Member>(identifier.MemberId);
+
+			if (member == null)
+				throw new ApplicationException("Expected valid member");
+
+			return member;
+		}
+
+		public Member FindMemberById(string id)
+		{
+			var member = _session.Load<Member>(id);
 
 			if (member == null)
 				throw new ApplicationException("Expected valid member");
