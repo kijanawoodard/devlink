ac9bd85ba195d231128f2ff000e2945b423420f5
Kijana Woodard <github@wyldeye.com>
2013-09-10 21:48:22 -0500

    change to webshim lib modernizr

--- src/DevLink.Public/App_Start/BundleConfig.cs
+++ src/DevLink.Public/App_Start/BundleConfig.cs
@@ -22,7 +22,7 @@ namespace DevLink.Public
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
 			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-						"~/Scripts/modernizr-*"));
+						"~/Scripts/js-webshim/minified/extras/modernizr-custom.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 			bundles.Add(new StyleBundle("~/Content/public/css").Include("~/Content/public.css"));
